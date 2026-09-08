6cdfa08a11abec5394d2614b0014ebec8f68ed6a
Beto Ramirez <maramirez10@gmail.com>
2026-09-08 17:00:54 -0600

    added angular material

--- src/app/shared/services/modal-service.ts
+++ src/app/shared/services/modal-service.ts
@@ -1,5 +1,5 @@
 import { inject, Service } from '@angular/core';
-// import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import {
   PageModalModel,
   PageModalType,
@@ -9,69 +9,69 @@ import { map, Observable } from 'rxjs';
 
 @Service()
 export class ModalService {
-  // private readonly dialog = inject(MatDialog);
-  //
-  // showErrorModal(title?: string, body?: string): void {
-  //   const type: PageModalType = 'Error';
-  //   if (!title) title = 'Error';
-  //   if (!body) body = 'Error';
-  //
-  //   this.showModal(title, body, type);
-  // }
-  //
-  // showConfirmationModal(title?: string, body?: string): void {
-  //   const type: PageModalType = 'Confirmation';
-  //   if (!title) title = 'Confirmacion';
-  //   if (!body) body = 'Alerta de confirmacion';
-  //   this.showModal(title, body, type);
-  // }
-  //
-  // showYesNoModal(
-  //   title?: string,
-  //   body?: string,
-  //   yesText?: string,
-  //   noText?: string,
-  // ): Observable<boolean> {
-  //   const type: PageModalType = 'YesNo';
-  //   if (!title) title = 'Respuesta';
-  //   if (!body) body = 'Seleccione una respuesta';
-  //   if (!yesText) yesText = 'Si';
-  //   if (!noText) noText = 'No';
-  //
-  //   return this.showModal(title, body, type, yesText, noText);
-  // }
-  //
-  // private showModal(
-  //   title: string,
-  //   body: string,
-  //   type: PageModalType,
-  //   yesText?: string,
-  //   noText?: string,
-  // ): Observable<boolean> {
-  //   let dialogRef: MatDialogRef<PageModal, any>;
-  //   if(type === 'YesNo') {
-  //     dialogRef = this.dialog.open<PageModal, PageModalModel, any>(PageModal, {
-  //       hasBackdrop: true,
-  //       disableClose: true,
-  //       data: {
-  //         type,
-  //         title,
-  //         body,
-  //         noText,
-  //         yesText,
-  //       },
-  //     });
-  //   } else {
-  //     dialogRef = this.dialog.open<PageModal, PageModalModel, any>(PageModal, {
-  //       hasBackdrop: true,
-  //       disableClose: true,
-  //       data: {
-  //         type,
-  //         title,
-  //         body,
-  //       },
-  //     });
-  //   }
-  //   return dialogRef.afterClosed().pipe(map((result) => result === true));
-  // }
+  private readonly dialog = inject(MatDialog);
+
+  showErrorModal(title?: string, body?: string): void {
+    const type: PageModalType = 'Error';
+    if (!title) title = 'Error';
+    if (!body) body = 'Error';
+
+    this.showModal(title, body, type);
+  }
+
+  showConfirmationModal(title?: string, body?: string): void {
+    const type: PageModalType = 'Confirmation';
+    if (!title) title = 'Confirmacion';
+    if (!body) body = 'Alerta de confirmacion';
+    this.showModal(title, body, type);
+  }
+
+  showYesNoModal(
+    title?: string,
+    body?: string,
+    yesText?: string,
+    noText?: string,
+  ): Observable<boolean> {
+    const type: PageModalType = 'YesNo';
+    if (!title) title = 'Respuesta';
+    if (!body) body = 'Seleccione una respuesta';
+    if (!yesText) yesText = 'Si';
+    if (!noText) noText = 'No';
+
+    return this.showModal(title, body, type, yesText, noText);
+  }
+
+  private showModal(
+    title: string,
+    body: string,
+    type: PageModalType,
+    yesText?: string,
+    noText?: string,
+  ): Observable<boolean> {
+    let dialogRef: MatDialogRef<PageModal, any>;
+    if(type === 'YesNo') {
+      dialogRef = this.dialog.open<PageModal, PageModalModel, any>(PageModal, {
+        hasBackdrop: true,
+        disableClose: true,
+        data: {
+          type,
+          title,
+          body,
+          noText,
+          yesText,
+        },
+      });
+    } else {
+      dialogRef = this.dialog.open<PageModal, PageModalModel, any>(PageModal, {
+        hasBackdrop: true,
+        disableClose: true,
+        data: {
+          type,
+          title,
+          body,
+        },
+      });
+    }
+    return dialogRef.afterClosed().pipe(map((result) => result === true));
+  }
 }
--- src/app/shared/ui/page-modal/page-modal.ts
+++ src/app/shared/ui/page-modal/page-modal.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, TemplateRef, viewChild } from '@angular/core';
 import { PageModalModel } from '@shared-ui/page-modal/page-modal-model';
 import { NgTemplateOutlet } from '@angular/common';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   imports: [NgTemplateOutlet],
@@ -9,26 +10,26 @@ import { NgTemplateOutlet } from '@angular/common';
   templateUrl: './page-modal.html',
 })
 export class PageModal {
-  // readonly data: PageModalModel = inject<PageModalModel>(MAT_DIALOG_DATA);
-  // readonly dialogRef = inject(MatDialogRef<PageModal>);
+  readonly data: PageModalModel = inject<PageModalModel>(MAT_DIALOG_DATA);
+  readonly dialogRef = inject(MatDialogRef<PageModal>);
 
   private readonly errorTemplate = viewChild('errorTemplate', { read: TemplateRef });
   private readonly confirmationTemplate = viewChild('confirmationTemplate', { read: TemplateRef });
   private readonly yesNoTemplate = viewChild('yesNoTemplate', { read: TemplateRef });
 
   templateType = computed(() => {
-    // if (this.data.type === 'Error') {
-    //   return this.errorTemplate();
-    // } else if (this.data.type === 'Confirmation') {
-    //   return this.confirmationTemplate();
-    // } else if (this.data.type === 'YesNo') {
-    //   return this.yesNoTemplate();
-    // } else {
+    if (this.data.type === 'Error') {
       return this.errorTemplate();
-    // }
+    } else if (this.data.type === 'Confirmation') {
+      return this.confirmationTemplate();
+    } else if (this.data.type === 'YesNo') {
+      return this.yesNoTemplate();
+    } else {
+      return this.errorTemplate();
+    }
   });
 
   protected onNoClick() {
-    // this.dialogRef.close();
+    this.dialogRef.close();
   }
 }
